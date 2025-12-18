ae993f0ca2569c30a987b92898e92a1a22388be3
fabricio861 <fabriciomansano340@gmail.com>
2025-12-17 22:31:39 -0300

    button
    só falta acrescentar que pode mudar as informações

--- pet-petrillo/src/app/page.tsx
+++ pet-petrillo/src/app/page.tsx
@@ -1,19 +1,12 @@
 import Image from "next/image";
 import { Navbar } from "@/components/navbar/page";
 import { CardServicos } from "@/components/cardServicos/page";
+import Button from "@/components/button/page";
 import { Droplet } from "lucide-react";
 
-export default function Home() {
+export default function () {
   return (
-  
-   <CardServicos
-        titulo="Banho"
-        descricao="Banho completo com produtos premium e secagem profissional"
-        preco={45}
-        duracao="45-60 minutos"
-        corDestaque="#4a9eff"
-        Icone={Droplet}
-        corFundo = "#bad3f0ff"
-      />
+
+   <Button/>
   );
 }
---
+++ pet-petrillo/src/components/button/page.tsx
@@ -0,0 +1,14 @@
+        import styles from '@/components/button/button.module.css';
+
+
+export default function Button(){
+    return(
+        <>
+        <div className={styles.fundobtn}>
+        <button className={styles.button}>
+        click aqui
+        </button>
+        </div>
+        </>
+    );
+}
