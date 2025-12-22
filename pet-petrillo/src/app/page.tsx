31705e9dcd787457621a40ef7b425cc1490d0b0e
fabricio861 <fabriciomansano340@gmail.com>
2025-12-22 16:40:11 -0300

    22/12

--- pet-petrillo/src/app/page.tsx
+++ pet-petrillo/src/app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import { Navbar } from "@/components/navbar/page";
 import { CardServicos } from "@/components/cardServicos/page";
-
-import Button from "@/components/button/page";
+import {Button} from "@/components/button/page";
 
 
 export default function () {
@@ -10,6 +9,10 @@ export default function () {
 
   
  <Button 
+    texto="click aqui"
+    corFundo="#rgb(11, 255, 88)"
+    corFundoHover="#rgba(47, 159, 83, 1)"
+    corText="white"
  />
 
 
