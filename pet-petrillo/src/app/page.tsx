9762c264762f5a00d45cdbea791c25a027fd3301
fabricio861 <fabriciomansano340@gmail.com>
2026-01-06 21:42:11 -0300

    6/01
    terminar components home

--- pet-petrillo/src/app/page.tsx
+++ pet-petrillo/src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';  
 import Home from './pages/home/page';
-
+import { Espaco } from '@/components/compohome/page';
 
 
 export default function 
 () {
   return (
 
-  <Home></Home> 
+  <Espaco></Espaco> 
 
 
 
---
+++ pet-petrillo/src/components/compohome/page.tsx
@@ -0,0 +1,17 @@
+
+import style from '@/components/compohome/homec.module.css'
+
+
+export function Espaco() {
+  
+    return (
+    < >
+     <div className={style.fundo}>
+      <div className={style.text}>
+        <h1>Cuidado completo para seu melhor amigo</h1>
+      </div>
+
+     </div>
+    </>
+  );
+}
